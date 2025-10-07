c3b997fb9e52c1e0ad10308324bec180655cdd43
Viviyaka <bess.iri.1224@gmail.com>
2025-10-07 21:44:44 +0200

    DU

---
+++ Seminar2new/Seminar2new/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace seminar2new
+{
+    internal class Programm
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 5, 6, 18, 7, 10, 22, 3, 19, 50, 31, 87, 16 };
+            int maxNumber = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] > maxNumber)
+                {
+                    maxNumber = numbers[i];
+                }
+            }
+            Console.WriteLine($"Maximalni cislo: {maxNumber}");
+            Console.WriteLine("");
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = 0; j < numbers.Length - 1; j++)
+                {
+                    if (numbers[j] < numbers[j + 1])
+                    {
+                        int tmp_value = numbers[j];
+                        numbers[j] = numbers[j + 1];
+                        numbers[j + 1] = tmp_value;
+                    }
+                }
+            }
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine($"{numbers[i]}");
+            }
+            Console.WriteLine("");
+            Console.Write("Napiste cislo: ");
+            int cNumber = int.Parse(Console.ReadLine());
+            int numberIndex = -1;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] == cNumber)
+                {
+                    numberIndex = i;
+                    break;
+                }
+            }
+
+            Console.WriteLine($"Index cisla {cNumber}: {numberIndex}");
+
+
+            while (true)
+            {
+
+            }
+        }
+    }
+}
---
+++ seminar1new/seminar1new/Program.cs
@@ -0,0 +1,90 @@
+﻿namespace seminar1new
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int studentsValue = 0;
+
+            Console.Write("Pocet studentu: ");
+            studentsValue = int.Parse(Console.ReadLine());
+
+            string[] studentsName = new string[studentsValue];
+            int[] studentsAge = new int[studentsValue];
+            float[] studentsMark = new float[studentsValue];
+
+            for (int i = 0; i < studentsValue; i++)
+            {
+                Console.WriteLine("---");
+                Console.Write($"Napiste jmeno studenta {i + 1}: ");
+                studentsName[i] = Console.ReadLine();
+                Console.Write($"Napiste vek studenta {i + 1}: ");
+                studentsAge[i] = int.Parse(Console.ReadLine());
+                Console.Write($"Napiste prumernou znamku studenta {i + 1}: ");
+                studentsMark[i] = float.Parse(Console.ReadLine());
+                Console.WriteLine("---");
+            }
+
+            StudentsList();
+
+        void StudentsList()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("a: Vsechny studenty ");
+            Console.WriteLine("b: Vsechny studenty ktere maji prumernou znamku lepsi nez 2 ");
+            Console.WriteLine("c: Prumerny vek vsech studentu ");
+            Console.WriteLine("d: Ukoncit program");
+            Console.Write("Vyberte co chcete udelat: ");
+
+            string worldValue = Console.ReadLine();
+
+            if (worldValue == "a")
+            {
+                for (int i = 0; i < studentsValue; i++)
+                {
+                    Console.WriteLine($"{studentsName[i]} ({studentsAge[i]}): {studentsMark[i]}");
+                }
+            }
+            else if (worldValue == "b")
+            {
+                for (int i = 0; i < studentsValue; i++)
+                {
+                    if (studentsMark[i] < 2.0)
+                    {
+                        Console.WriteLine($"{studentsName[i]} ({studentsAge[i]}): {studentsMark[i]}");
+
+                    }
+                }
+            }
+            else if (worldValue == "c")
+            {
+                float middleAge = 1.0f;
+                int summAge = 0;
+
+                for (int i = 0; i < studentsValue; i++)
+                {
+                    summAge += studentsAge[i];
+                }
+
+                middleAge = summAge / studentsValue;
+                Console.WriteLine($"Prumerny vek: {middleAge}");
+
+
+            }
+            else if (worldValue == "d")
+            {
+                Environment.Exit(0);
+            }
+
+            if (worldValue != "d")
+            {
+                StudentsList();
+            }
+        }
+
+            while (true)
+            {
+            }
+        }
+    }
+}
