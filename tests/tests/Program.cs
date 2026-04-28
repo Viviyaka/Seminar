f29199326c059f4c59715a940ecce4491d515838
Vivyaka <bess.iri.1224@gmail.com>
2026-04-28 16:33:49 +0200

    test

---
+++ tests/tests/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace tests
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pocet = Convert.ToInt32(Console.ReadLine());
+            int[] cesty = Convert.ToInt32(Console.ReadLine().Split(" "));
+            int[] dvojice = new int (cesty);
+            for (int i = 0; i < dvojice.Length; i++)
+            {
+                string[] seznamDvojice = dvojice[i];
+
+                int a = Convert.ToInt32(seznamDvojice[0]) - 1;
+                int b = Convert.ToInt32(seznamDvojice[1]) - 1;
+                int[,] matice = new int[pocet, pocet];
+                matice[a, b] = 1;
+                matice[b, a] = 1;
+            }
+
+            
+        }
+    }
+}
